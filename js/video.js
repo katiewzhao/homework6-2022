c3d75eb8fd5d38bac3892e0f9b0efe8e75bf92be
katiewzhao <78450721+katiewzhao@users.noreply.github.com>
2022-04-14 18:44:20 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -4,7 +4,6 @@ window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 	video.autoplay = false
 	video.loop = false
-	console.log("The current volume is " + video.volume)
 });
 
 document.querySelector("#play").addEventListener("click", function() {
